07888198188cb6a0cd3e64c8f7ea4c79dc582fc2
Ali Azizi Pour <aliap1376ir@gmail.com>
2021-03-15 12:08:24 +0330

    init

---
+++ src/main/java/ir/aliap1376ir/sources/protobuf/server/ProtobufApplication.java
@@ -0,0 +1,21 @@
+package ir.aliap1376ir.sources.protobuf.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ProtobufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufApplication.class, args);
+    }
+
+    @Bean
+    public ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
+}
