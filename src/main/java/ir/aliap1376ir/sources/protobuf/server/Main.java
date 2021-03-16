86582fe211d5d2f359aae88e7aa397cd0bdd49c7
Ali Azizi Pour <aliap1376ir@gmail.com>
2021-03-16 19:15:19 +0330

    sync with android part finished!

--- src/main/java/ir/aliap1376ir/sources/protobuf/server/Main.java
+++ src/main/java/ir/aliap1376ir/sources/protobuf/server/Main.java
@@ -2,20 +2,12 @@ package ir.aliap1376ir.sources.protobuf.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @Bean
-    public ProtobufHttpMessageConverter protobufHttpMessageConverter() {
-        return new ProtobufHttpMessageConverter();
-    }
 }
--- src/main/java/ir/aliap1376ir/sources/protobuf/server/controller/Controller.java
+++ src/main/java/ir/aliap1376ir/sources/protobuf/server/controller/Controller.java
@@ -1,4 +1,4 @@
-package ir.aliap1376ir.sources.protobuf.server.Controller;
+package ir.aliap1376ir.sources.protobuf.server.controller;
 
 import ir.aliap1376ir.sources.protobuf.server.object.PersonOuterClass;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +11,17 @@ public class Controller {
 
     PersonOuterClass.People people = PersonOuterClass.People.newBuilder().build();
 
-    @PostMapping(consumes = "application/x-protobuf", produces = "application/x-protobuf")
+    @PostMapping(path = "/register", consumes = "application/x-protobuf", produces = "application/x-protobuf")
     private PersonOuterClass.Person registerPerson(@RequestBody PersonOuterClass.Person person) {
         person = person.toBuilder().setId(people.getPersonCount()).build();
         people = people.toBuilder().addPerson(person).build();
+        System.out.println("Client Register a new one");
         return person;
     }
 
-    @GetMapping(consumes = "application/x-protobuf", produces = "application/x-protobuf")
+    @GetMapping(path = "/all", consumes = "application/x-protobuf", produces = "application/x-protobuf")
     private PersonOuterClass.People getAllPeople() {
+        System.out.println("Client Request for all!");
         return people;
     }
 }
